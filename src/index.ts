7afdec40aa9357849ae66df0f6a6aa024d4ce1be
Kevin Schans <kevinvdsd@hotmail.com>
2023-06-08 22:19:02 +0200

    Add calculator functions

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,52 @@
-import http, { request } from "http";
+import http from "http";
 import url from 'url';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 const PORT = process.env.PORT || 4545;
 
-const server = http.createServer((request, response)) => {
-  if (request.url !== 'calculator') {
-    return response.write(`<p>ERROR! Path empty</p>`);
+const server = http.createServer((request, response) => {
+  function calculator(a: any, b: any) {
+    const firstNumber = Number(a);
+    const secondNumber = Number(b);
+
+    const add: number = firstNumber + secondNumber;
+    const substract: number = firstNumber - secondNumber;
+    const multiply: number = firstNumber * secondNumber;
+    const division: number = firstNumber / secondNumber;
+
+    return {add, substract, multiply, division}
   }
 
-  const { pathname } = url.parse(request.url);
+    if (!request.url) {
+      server.emit('ERROR', new Error('Error 404'));
+      response.write(`<span>Error 404</span>`);
+      return;
+    }
 
-  response.write(`
-  <span>RESULTS</span>
-  <span>${pathname!}</span>`)
+    const { pathname, search } = url.parse(request.url);
 
-  Response.write(request.method);
-  response.write(request.url);
+    if (pathname !== '/calculator') {
+      server.emit('ERROR', new Error('Cannot find path'));
+      response.write(`<span>Error 404</span>`);
+      return;
+    }
+
+    if (pathname === '/calculator') {
+      const urlParams = new URLSearchParams(search!);
+      const firstNumber = urlParams.get('a');
+      const secondNumber = urlParams.get('b');
+
+      const answers = calculator(firstNumber!, secondNumber!);
+      response.write(`
+      RESULTS
+      <p>${firstNumber} + ${secondNumber} = ${answers.add}</p>
+      <p>${firstNumber} - ${secondNumber} = ${answers.substract}</p>
+      <p>${firstNumber} * ${secondNumber} = ${answers.multiply}</p>
+      <p>${firstNumber} / ${secondNumber} = ${answers.division}</p>
+      `);
+    }
+
   response.end();
-
 });
+
+server.on('ERROR', () => {});
+server.listen(PORT);
